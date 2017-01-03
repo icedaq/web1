4e3a258208f5e6c902502341a9cb523c84f5e3f6
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-03 13:48:20 +0100

    Fixes session storage, makes use of the cartitem class and adds some more functions.

--- src/model/ShoppingCart.php
+++ src/model/ShoppingCart.php
@@ -2,14 +2,15 @@
 
 session_start();
 
+require_once('ShoppingCartItem.php');
+
 // The shopping cart.
 class ShoppingCart {
 
 	private $cart = array(); 
 
-	public function load() {
-
-		if (!isempty($_SESSION["shoppingcart"])) {
+	public static function load() {
+		if (!empty($_SESSION["shoppingcart"])) {
 			return $_SESSION["shoppingcart"];
 		} else {
 			$newCart = new ShoppingCart();
@@ -18,31 +19,48 @@ class ShoppingCart {
 		}
 	}
 
-	public function addProduct($productID, $quantity) {
-		array_push($this->cart, ["productID"=>$productID, "quantity"=>$quantity]);
+    public function addProduct($productID, $quantity) {
+        $item = new ShoppingCartItem($productID, $quantity);
+		array_push($this->cart, $item);
 	}
 
 	public function updateProduct($productID, $newQuantity) {
 		foreach ($this->cart as &$value) {
 
-			if ($value["productID"] == $productID)
+			if ($value->getId() == $productID)
 			{
-				$value["quantity"] = $newQuantity;
+				$value->setAmount($newQuantity);
 			}
 		}
 	}
 
-	public function getPosOfProduct($id) {
-		foreach ($this->cart as $key => $value) {
-			if ($value["productID"] == $id)
+	public function productQuantity($productID) {
+		foreach ($this->cart as &$value) {
+			if ($value->getId() == $productID)
 			{
-				return $key;
+				return $value->getAmount();
 			}
 		}
 	}
-
+    
 	public function getCart() {
 		return $this->cart;
 	}
+
+	// Calculate the price of the whole cart.
+	public function cartPrice() {
+
+		$price = 0;
+		foreach ($this->cart as $value) {
+            $price += $value->getPrice();
+		}
+    
+        return $price;
+
+    }
+
+    public function clearCart() {
+        $this->cart = array();
+    }
 
 }
