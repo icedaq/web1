043c13effbaf4edaaa37f224c2e639f99ba82a50
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-06 14:47:00 +0100

    Adds front-controller

--- src/index.php
+++ src/index.php
@@ -1,12 +1,9 @@
 <?php
 
-require("model/Database.php");
-
-//$db=Database::getInstance();
-//$db->seed();
-//echo $db->debug();
-
-// Just a test if the rewriting works.
-echo $_SERVER['REQUEST_URI'];
+// Front-Controller with controller-centric routing.
+$controllerclass = $_GET['controller'];
+$controller = new $controllerclass();
+if (isset($_GET['action']))
+$controller->{$_GET['action']}();
 
 ?>
