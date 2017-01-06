2872469d09e66fa2d2bafc025deee8f38a219286
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-06 14:23:16 +0100

    Adds to index.php to test url rewriting.

--- src/index.php
+++ src/index.php
@@ -2,8 +2,11 @@
 
 require("model/Database.php");
 
-$db=Database::getInstance();
-$db->seed();
-echo $db->debug();
+//$db=Database::getInstance();
+//$db->seed();
+//echo $db->debug();
+
+// Just a test if the rewriting works.
+echo $_SERVER['REQUEST_URI'];
 
 ?>
