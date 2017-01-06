1bc5dbbeea6dfbf45b42247739fb1983a8132f4d
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-06 18:05:00 +0100

    Adds translation dictionary

---
+++ src/helpers/translator.php
@@ -0,0 +1,30 @@
+<?php
+
+if (isset($_GET['language'])) {
+    // TODO: Set cookie.
+}
+
+function t($key) {
+    
+    if (isset($_COOKIE['language'])) {
+        $language = $_COOKIE['language'];  
+    } else {
+        $language = "en";
+    }
+
+    $texts = array(
+        'page' => array(
+        'de'=>'Seite',
+        'en'=>'Page' ),
+        
+        'houseNr' => array(
+        'de'=>'Hausnummer',
+        'en'=>'House number' ),
+
+        'content' => array(
+        'de'=>'Willkommen auf der Seite ',
+        'en'=>'Welcome to the page '));
+        return isset($texts[$key][$language])
+        ? $texts[$key][$language]
+        : "[$key]";
+}
