8353a0f347fcde9df145b656ec73ed078faae996
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-06 21:27:13 +0100

    Adds the view and controller for the catalog.

---
+++ src/controller/catalogController.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+
+require_once("model/Catalog.php");
+require_once("view/CatalogView.php");
+
+// This class is used to manage the products.
+class catalogController {
+
+    private $model;
+    private $view;
+    private $id;
+
+    public function __construct() {
+        $this->model = new Catalog();
+        $this->view = new CatalogView();
+
+        // Make sure there is some data in the database.
+        $db = Database::getInstance(); 
+        if ($db->isEmpty()) {
+            $db->seed();
+        }
+    }
+
+    public function setId($id) {
+        $this->id =$id;
+    }
+
+    public function consumeId() {
+        $theid = $this->id;
+        $this->id = null;
+        return $theid;
+    }
+
+    // For the jquery UI autocomplete.
+    public function search() {
+
+        if(isset($_POST['term'])) {
+
+            $products = $this->model->filterProductsByName($_POST['term']);
+
+            $result = array();
+		    foreach ($products as $value) {
+                array_push($result,array('label'=>$value->getName(),'value'=>$value->getId()));
+            }
+
+            echo(json_encode($result));
+        }  
+    }
+
+    public function show() {
+        if ($this->id == null)
+        {
+            // TODO: Show all products.
+            echo "Showing all products.";
+        } else {
+            $id = $this->consumeId();
+            // TODO: Show only one product.
+            echo "Showing product number: ".$id;
+        }
+    }
+
+    
+}
