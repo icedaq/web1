651383320973a790d5035e9303b589c1ba0431a7
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-06 14:59:52 +0100

    frontend-controller now also loads the required file.

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 
 // Front-Controller with controller-centric routing.
 $controllerclass = $_GET['controller'];
+require_once("controller/".$controllerclass.".php");
 $controller = new $controllerclass();
 if (isset($_GET['action']))
 $controller->{$_GET['action']}();
