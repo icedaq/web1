3d9ee8ff71b16c32c7806edf5a6aef4d471be0fb
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-06 21:26:45 +0100

    Adds a first basic version of the controller and view for the shopping cart.

---
+++ src/controller/cartController.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+require_once("model/ShoppingCart.php");
+require_once("view/CartView.php");
+
+// This class is used to manage the products.
+class cartController {
+
+    private $model;
+    private $view;
+    private $id;
+    private $params;
+
+    public function __construct() {
+        $this->model = new ShoppingCart();
+        $this->view = new CartView();
+    }
+
+    public function setId($id) {
+        $this->id =$id;
+    }
+
+    public function setParams($params) {
+        $this->params =$params;
+    }
+
+    public function consumeId() {
+        $theid = $this->id;
+        $this->id = null;
+        return $theid;
+    }
+
+    public function add() {
+        if (isset($this->params['prodId'])) {
+            echo "jes";
+        }
+    }
+    
+}
---
+++ src/view/CartView.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("helpers/translator.php");
+
+class CartView {
+
+    // Empty at the moment.
+
+}
