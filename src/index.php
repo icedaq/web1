dcb634137881cfd945126d8bc294b1ad69794565
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-06 21:25:44 +0100

    Changes the .htaccess file so that the urls get parsed in the php code and not with a regex directly inside the .htaccess file.

--- src/index.php
+++ src/index.php
@@ -1,10 +1,35 @@
 <?php
 
-// Front-Controller with controller-centric routing.
-$controllerclass = $_GET['controller'];
-include_once("controller/".$controllerclass.".php");
-$controller = new $controllerclass();
-if (isset($_GET['action']))
-$controller->{$_GET['action']}();
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$components = explode('/', $path);
 
+// Front-Controller with controller-centric routing.
+$controllerclass = $components[1]."Controller";
+
+include_once("controller/".$controllerclass.".php");
+
+$controller = new $controllerclass();
+
+if (isset($components[3])) {
+    $controller->setId($components[3]);
+
+} 
+
+$gets = explode('?', $_SERVER['REQUEST_URI'])[1];
+if (isset($gets)) {
+
+    $params = array(); 
+    $pairs = explode('&', $gets);
+
+    foreach ($pairs as $p) {
+        $split = explode('=', $p);
+        $params[$split[0]] = $split[1];
+    }
+
+    $controller->setParams($params);
+}
+
+if (isset($components[2])) {
+    $controller->{$components[2]}();
+}
 ?>
