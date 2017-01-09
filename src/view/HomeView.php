020ac387a931be0d9c96f35c53936baf353a459f
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-09 22:20:49 +0100

    Changes call the renderNavigation to highlight current item.

--- src/view/HomeView.php
+++ src/view/HomeView.php
@@ -8,7 +8,7 @@ class HomeView {
     public function renderHome() {
 
         MainView::renderMeta("Home"); // Param is title of the page.
-        MainView::renderNavigation();
+        MainView::renderNavigation("home");
         $this->renderContent();         
         MainView::renderFooter(); 
     }
