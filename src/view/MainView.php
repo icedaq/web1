384f87a48104edaa1763ddbe43a56bd7489de55d
Louis Justus Siegrist <louis.siegrist@gmx.net>
2017-01-11 12:12:32 +0100

    add viewport to header

--- src/view/MainView.php
+++ src/view/MainView.php
@@ -14,6 +14,7 @@ class MainView
                     <html lang="de">
                         <head>
                         <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0"
                         <title>ApertureLab - ' . $title . '</title>
                         <link rel="icon" href="../favicon.ico" type="image/x-icon">
                         <link rel="stylesheet" href="/css/reset.css">
