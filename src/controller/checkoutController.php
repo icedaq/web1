7a610012955de537a0d4ce1c1cba0e6250ed0908
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-13 00:00:44 +0100

    Adds code to handle inputs on checkout forms.

--- src/controller/checkoutController.php
+++ src/controller/checkoutController.php
@@ -1,5 +1,7 @@
 <?php
+require_once("model/ShoppingCart.php");
 require_once("model/OrderManager.php");
+require_once("model/UserManager.php");
 require_once("view/CheckoutView.php");
 session_start();
 
@@ -9,11 +11,11 @@ class checkoutController {
     private $model;
     private $view;
     private $id; // This is the current step.
+    
 
     public function __construct() {
         $this->model = new OrderManager();
         $this->view = new CheckoutView($this->model);
-
     }
 
     public function setId($id) {
@@ -30,6 +32,32 @@ class checkoutController {
             header("Location: /checkout/step/1");
             exit();
         } else {
+
+            // Second step
+            if (($this->id == 2) && (UserManager::isLoggedIn()))
+            {
+                if(isset($_POST['deliveryMethod']) 
+                    &&  isset($_POST['paymentMethod'])
+                    &&  isset($_POST['giftbox'])) {
+                    
+                    $this->model->addOrder();
+                    $order = $this->model->getCurrentOrder();
+                    $order->setShippment($_POST['deliveryMethod']); 
+                    $order->setPayment($_POST['paymentMethod']); 
+                    $order->setGift($_POST['giftbox']); 
+                }
+            }
+
+            // Third step
+            if (($this->id == 3) && (UserManager::isLoggedIn()))
+            {
+                if(isset($_POST['comment'])) {
+                    $order = $this->model->getCurrentOrder();
+                    $order->setComment($_POST['comment']);
+                    $order->setCommited(); 
+                }
+            }
+
             $this->view->{"renderStep".$this->getId()}();
         }
     }
