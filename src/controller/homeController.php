0498206ed74bcf4fbee9c69657b9cb109b739867
Pascal Liniger <pascal.liniger@gmail.com>
2017-01-12 21:13:32 +0100

    Removes unused stuff.

--- src/controller/homeController.php
+++ src/controller/homeController.php
@@ -1,6 +1,5 @@
 <?php
 require_once("view/HomeView.php");
-session_start();
 
 // This class is used to manage the products.
 class homeController {
@@ -9,11 +8,10 @@ class homeController {
     private $view;
 
     public function __construct() {
-        //$this->model = new UserManager();
         $this->view = new HomeView();
     }
 
     public function home() {
-            $this->view->renderHome();
+        $this->view->renderHome();
     }
 }
